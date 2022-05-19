8e255eaeedaa1e0c45649d0998c99e588f419471
JovanniJovanni <trmcburger@gmail.com>
2022-05-19 17:50:02 -0400

    rgb - hsv the only useful ones

--- src/Color/ColorUtils.lua
+++ src/Color/ColorUtils.lua
@@ -3,41 +3,66 @@
 ]=]
 local ColorUtils = {}
 
-type HSV = {
+export type HSV = {
 	h : number,
 	s : number,
 	v : number
 }
 
-type RGB = {
+export type RGB = {
 	r : number,
 	g : number,
 	b : number
 }
 
 
-function ColorUtils.RBGtoHex(color : RGB) : string
-	local r, g, b = tostring(color.r), tostring(color.g), tostring(color.b)
-	
-	if #r == 1 then
-		r = "0" .. r
-	end
-	if #g == 1 then
-		g = "0" .. g
-	end
-	if #b == 1 then
-		b = "0" .. b
-	end
+function ColorUtils.RBGtoHSV(rgb : RGB) : HSV
+	local maxc = math.max(unpack(rgb))
+	local minc = math.min(unpack(rgb))
+	local v = maxc
 
-	return ("#%s%s%s"):format(r,g,b)
+	if minc == maxc then return {h = 0, s = 0, v = v} end
+
+	local s = (maxc - minc) / maxc
+	local rc = (maxc - rgb.r) / (maxc - minc)
+	local gc = (maxc - rgb.g) / (maxc - minc)
+	local bc = (maxc - rgb.b) / (maxc - minc)
+
+	local h
+
+	if rgb.r == maxc then 
+		h = bc - gc
+	elseif rgb.g == maxc then
+		h = 2 + rc - bc
+	else
+		h = 4 + gc - rc
+	end
+	h /= 6
+
+	return {h = h, s = s, v = v}
 end
 
-function ColorUtils.HextoRGB(hex : string) : RGB
-	if #hex == 4 then
-		return 
-	end
 
-	return {}
+function ColorUtils.HSVtoRGB(hsv : HSV) : RGB
+	if hsv.s == 0 then return {r = hsv.v, g = hsv.v, b = hsv.v} end
+
+	local i = math.floor(hsv.h * 6)
+	local f = hsv.h * 6 - i
+	local p = hsv.v * (1 - hsv.s)
+	local q = hsv.v * (1 - f * hsv.s)
+	local t = hsv.v * (1 - hsv.s * (1 - f))
+	i %= 6
+
+	local HSVtoRGBswitch = {
+		{r = hsv.v, g = t, b = p},
+		{r = q, g = hsv.v, b = p},
+		{r = p, g = hsv.v, b = t},
+		{r = p, g = q, b = hsv.v},
+		{r = t, g = p, b = hsv.v},
+		{r = hsv.v, g = p, b = q}
+	}
+
+	return HSVtoRGBswitch[i + 1] -- +1 because tables are 1-indexed
 end
 
 return ColorUtils
